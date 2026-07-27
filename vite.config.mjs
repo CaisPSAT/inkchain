32557ed38ce3d4dbb15e9866166b1485a6e02d5f
CaisPSAT <Cais2108@gmail.com>
2026-07-26 19:20:51 -0500

    Fix project folder structure

--- vite.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  optimizeDeps: {
-    include: ["@vitejs/plugin-react", "react", "react/jsx-dev-runtime", "react-dom/client", "qrcode.react", "socket.io-client"]
-  }
-});
